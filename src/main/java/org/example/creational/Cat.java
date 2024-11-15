033605bcb33027a6ebc5b93a157dddb8c70d3cc7
Manuel <mantel1337@gmail.com>
2024-11-15 14:03:20 +0100

    builder pattern

--- src/main/java/org/example/creational/Cat.java
+++ src/main/java/org/example/creational/Cat.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.creational;
 
 public class Cat {
 
--- src/test/java/org/example/creational/CatTest.java
+++ src/test/java/org/example/creational/CatTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.creational;
 
 import org.junit.jupiter.api.Test;
 
