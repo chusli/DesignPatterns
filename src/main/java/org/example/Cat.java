834bd233c36993448bf97fcc80908fc4445c2e0b
Manuel <mantel1337@gmail.com>
2024-11-15 14:01:03 +0100

    builder pattern

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,78 @@
+package org.example;
+
+public class Cat {
+
+    private final String name;
+
+    private final String fur;
+
+    private final String attitude;
+
+    private Cat(Builder catBuilder) {
+        name = catBuilder.name;
+        fur = catBuilder.fur;
+        attitude = catBuilder.attitude;
+    }
+
+    public static FurBuilder withName(String name) {
+        return new Builder(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFur() {
+        return fur;
+    }
+
+    public String getAttitude() {
+        return attitude;
+    }
+
+    public sealed interface FurBuilder permits Builder {
+
+        CatCreator withFur(String fur);
+
+    }
+
+    public sealed interface CatCreator permits Builder {
+
+        CatCreator withAttitude(String attitude);
+
+        Cat build();
+
+    }
+
+    private static final class Builder implements FurBuilder, CatCreator {
+
+        private final String name;
+
+        private String fur;
+
+        private String attitude;
+
+        public Builder(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public CatCreator withFur(String fur) {
+            this.fur = fur;
+            return this;
+        }
+
+        @Override
+        public CatCreator withAttitude(String attitude) {
+            this.attitude = attitude;
+            return this;
+        }
+
+        @Override
+        public Cat build() {
+            return new Cat(this);
+        }
+
+    }
+
+}
---
+++ src/test/java/org/example/CatTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CatTest {
+
+    @Test
+    void createCat() {
+        Cat cat = Cat.withName("Stan")
+                .withFur("black and white")
+                .build();
+
+        assertThat(cat.getFur()).isEqualTo("black and white");
+        assertThat(cat.getName()).isEqualTo("Stan");
+    }
+
+    @Test
+    void createCatOptional() {
+        Cat cat = Cat.withName("Stan")
+                .withFur("black and white")
+                .withAttitude("angry")
+                .build();
+
+        assertThat(cat.getAttitude()).isEqualTo("angry");
+    }
+
+}
